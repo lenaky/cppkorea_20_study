4b01afca75315a13b9f529ee03f96eb4311319a9
jay.shin <jay.shin@linecorp.com>
2020-02-10 11:06:52 +0900

    주석 추가

--- ranges/example3/example3.cpp
+++ ranges/example3/example3.cpp
@@ -11,13 +11,14 @@ int main()
         });
     });
 
+    // 종료 조건 명시. Lazy evaluation 할때 종료조건이 존재하지 않으면 무한루프에 빠짐.
+    auto quit_cond = ranges::view::take_while([](std::tuple<int, int, int> nums) {
+        return std::get<0>(nums) < 100;
+    });
+
     // Lazy evaluation
-    ranges::for_each(
-        samzo | ranges::view::take_while([](std::tuple<int, int, int> nums) {
-            return std::get<0>(nums) < 100;
-        }),
-        [](std::tuple<int, int, int> nums) {
-            std::cout << std::get<0>(nums) << ", " << std::get<1>(nums) << ", " << std::get<2>(nums) << std::endl;
-        });
+    ranges::for_each(samzo | quit_cond, [](std::tuple<int, int, int> nums) {
+        std::cout << std::get<0>(nums) << ", " << std::get<1>(nums) << ", " << std::get<2>(nums) << std::endl;
+    });
     return 0;
 }
